a1ea535e2934fdb911ea683e4348f970ada5cc7f
igor mfe <igormafelipe@gmail.com>
2023-12-11 14:23:29 -0500

    changing backend to use sockets and sessions

--- app.py
+++ app.py
@@ -1,17 +1,77 @@
-from flask import Flask, jsonify, session
+from flask import Flask, jsonify, request
 from gameManager import gameManager
+from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_cors import CORS
-from flask_socketio import SocketIO
-# from flask_cors import CORS
-# from engineio.async_drivers import gevent
 
-app = Flask(__name__,
-            static_folder="./dist/static",
-            template_folder="./dist")
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'lol'
+
 manager = gameManager()
 
-# Allow CORS for all routes under /
-socketio = SocketIO(app, cors_allowed_origins="*")
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(cors_allowed_origins="*", async_mode='eventlet')
+socketio.init_app(app)
+
+# Socketio functions
+
+# user_id room: Used for private communications between server and user
+
+@socketio.on('connect')
+def handle_connect():
+    user_id = request.sid
+    join_room(user_id)
+    emit('on_connect', 'connected')
+    
+@socketio.on('disconnect')
+def handle_disconnect():
+    user_id = request.sid
+    leave_room(user_id)
+    emit('on_disconnect', 'disconnected')
+    
+# Rooms can have at most 2 players
+rooms = {}
+
+# User can only join a room that has 1 player in it.
+# If you join a room, your symbol is automatically O
+@socketio.on('join_room')
+def join_room_(data):
+    id = data["id"]
+    user_id = request.sid
+    if id not in rooms:
+        emit("room not found", to=user_id)
+    elif rooms[id] == 1:
+        join_room(id)
+        emit("room joined", to=user_id)
+    else:
+        emit("unable to join room", to=user_id)
+
+# If you create a room, your symbol is automatically X        
+@socketio.on('create_room')
+def create_room():
+    room_id = manager.generate_code()
+    user_id = request.sid
+    while room_id in rooms:
+        room_id = manager.generate_code()
+        
+    rooms[room_id] = 1
+    join_room(room_id)
+    emit({room_id: room_id}, to=user_id)
+
+# get the board once it is updated
+# player can only add a symbol to a coordinate, so pass x, y, and player
+@socketio.on('board_update')
+def board_update(data):
+    data_to_send = {"x":data["x"], "y":data["y"], "player":data["player"]}
+    emit("board_updated", data_to_send)
+
+# on the client, would do this:
+# socket.on('switch_turns', function() {
+# });
+@socketio.on('switch_turn')
+def switch_turn():
+    emit('switch_turns')
+
+# End points
 
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -47,4 +107,4 @@ def make_move():
     pass
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, port=5000)
