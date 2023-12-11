d035e8b525e08d77759df10bd6baf6f701bde110
igor mfe <igormafelipe@gmail.com>
2023-12-10 19:42:38 -0500

    adding home, new_game and end_game routes

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from gameManager import create_game, remove_game
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Flask app."
+    return "Welcome to big tic tac toe"
+
+@app.route('/new_game')
+def new_game():
+    return create_game()
+
+@app.route('/end_game')
+def end_game(code):
+    return remove_game(code)
 
 if __name__ == '__main__':
     app.run()
---
+++ gameManager.py
@@ -0,0 +1,32 @@
+import string
+import random
+from bigTicTacToe import BigTicTacToeBoard
+
+N=7
+
+class gameManager:
+    def __init__(self):
+        self.games = {}
+        
+    def generate_code(self):
+        code = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k=N))
+        
+        while code not in self.games:
+            code = self.generate_code()
+        
+        return code
+        
+    def create_game(self):
+        code = self.generate_code()
+        self.games[code] = BigTicTacToeBoard()
+        
+        return code
+    
+    def remove_game(self, code):
+        if not code:
+            return
+
+        self.games[code] = None
+            
+        
