12924ac4a0535bdcc33e266c465952dde2b61f6f
igor mfe <igormafelipe@gmail.com>
2023-12-22 14:17:51 -0500

    working server socket & rest API

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ socketio = SocketIO(cors_allowed_origins="*", async_mode='eventlet')
 socketio.init_app(app)
 
 # Socketio functions
-
 # user_id room: Used for private communications between server and user
 
 @socketio.on('connect')
@@ -31,28 +30,25 @@ rooms = {}
 # If you join a room, your symbol is automatically O
 @socketio.on('join_game')
 def join_game(data):
+    print("join_game\t", data, "\t", request.sid)
     id = data.get('id')
     user_id = request.sid
     if not id:
-        emit("Must specify an ID", to=user_id) 
+        emit("game_not_found", to=user_id) 
         return
     
     if id not in rooms:
         emit("game_not_found", to=user_id)
+    elif rooms[id] == 0:
+        join_room(id)
+        rooms[id] = 1
+        emit("player_1_joined", id, to=id)
     elif rooms[id] == 1:
         join_room(id)
-        emit("game_joined", id, to=id)
+        rooms[id] = 2
+        emit("player_2_joined", id, to=id)
     else:
         emit("unable_to_join_game", to=user_id)
-
-# If you create a room, your symbol is automatically X        
-@socketio.on('create_game')
-def create_game():
-    room_id = manager.create_game()
-    rooms[room_id] = 1
-    join_room(room_id)
-    print(room_id)
-    emit("game_created", room_id, to=room_id)
 
 @socketio.on('make_move')
 def update_board(data):
@@ -76,7 +72,29 @@ def update_board(data):
 # End points
 @app.route('/', methods=["GET", "POST"])
 def home():
-    return "<h1>Welcome to big tic tac toe<h1>"
+    return "Welcome to the tic-tac-toe backend! This is a flask server."
+
+@app.route('/create_game', methods=["POST"])
+def create_game():
+    room_id = manager.create_game()
+    rooms[room_id] = 0
+    return jsonify({"status": "success", "room_id": room_id})
+
+@app.route('/join_game', methods=["POST"])
+def join_game():
+    request_data = request.get_json()
+    room_id = request_data.get("room_id", None)
+    
+    if not room_id:
+        return jsonify({"status": "failure", "message": "No room id provided"})
+    
+    if not room_id in rooms:
+        return jsonify({"status": "failure", "message": "Room does not exist"})
+    
+    if rooms[room_id] != 1:
+        return jsonify({"status": "failure", "message": "Room is full"})
+    
+    return jsonify({"status": "success", "message": "Successfully joined room " + room_id})
 
 if __name__ == '__main__':
     socketio.run(app, port=5000)
