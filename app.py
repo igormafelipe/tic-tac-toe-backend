e2ef77d8d1a100aff48154f7561ac2d9e9a88081
igor mfe <igormafelipe@gmail.com>
2023-12-28 11:21:45 -0500

    removed secret key

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify, request
 from gameManager import gameManager
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask_socketio import SocketIO, emit, join_room
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'lol'
 
 manager = gameManager()
 
